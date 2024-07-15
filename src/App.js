c3b04cd8343e6cea054eb4ac6e2d750afc416e26
PraveenKumar263 <PraveenKumar263>
2024-07-15 21:59:25 +0800

    Establish routes for homepage & cart

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import productsData from './Data/products.json';
+import { ButtonAppBar } from './components/AppBar';
+import { CartProvider } from './components/CartContext';
+import { CartPage } from './components/CartPage.js';
+import { ProductsList } from './components/ProductsList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CartProvider>
+      <div className="App">
+        <ButtonAppBar />
+        <Routes>
+          <Route path="/" exact element={<ProductsList products={productsData.products} />} />
+          <Route path="/cart" element={<CartPage />} />
+        </Routes>
+      </div>
+    </CartProvider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
+
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
